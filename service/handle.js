30df559c29d5b356141ec95b05d27fdbfdc95f4a
Ed <yide1986@gmail.com>
2022-05-26 01:52:15 +0800

    feat: 新增自訂可預期錯誤 appError

--- service/handle.js
+++ service/handle.js
@@ -1,12 +1,4 @@
 const handle = {
-  successHandle(res, message, data) {
-    res.send({
-      "status": "success",
-      "message": message,
-      "data": data
-    });
-    res.end();
-  },
   errorHandle(res, message) {
     res.status(400).send({
       "status": "false",
